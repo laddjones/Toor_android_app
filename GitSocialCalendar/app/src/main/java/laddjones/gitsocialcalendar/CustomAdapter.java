e481d40aef7ba6edbb95e040fa34538952558d2f
laddjones <laddjonesiv@gmail.com>
2017-07-24 20:14:40 -0400

    adding android files

---
+++ GitSocialCalendar/app/src/main/java/laddjones/gitsocialcalendar/CustomAdapter.java
@@ -0,0 +1,156 @@
+package laddjones.gitsocialcalendar;
+
+/**
+ * Created by laddjones on 7/12/17.
+ */
+import java.util.List;
+
+import android.graphics.Color;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+
+public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.CustomViewHolder> {
+
+    private List<String> values;
+
+    public CustomAdapter(List<String> myDataSet) {
+        values = myDataSet;
+    }
+
+    public class CustomViewHolder extends RecyclerView.ViewHolder {
+        public TextView txtPersonName;
+        public TextView txtNumPeople;
+        public TextView txtetDescription;
+        public TextView txtDate;
+        public TextView txtTime;
+        public ImageView picOfP;
+        public View vrtLine;
+        public View hrLine;
+        public View layout;
+
+        public CustomViewHolder(View v) {
+            super(v);
+            layout = v;
+            picOfP = (ImageView) v.findViewById(R.id.picture);
+            txtPersonName = (TextView) v.findViewById(R.id.name);
+            txtNumPeople = (TextView) v.findViewById(R.id.numPeople);
+            txtetDescription = (TextView) v.findViewById(R.id.etDescription);
+            txtDate = (TextView) v.findViewById(R.id.dateTop);
+            txtTime = (TextView) v.findViewById(R.id.timeBelow);
+            vrtLine = v.findViewById(R.id.verticalLine);
+            hrLine = v.findViewById(R.id.horizontalLine);
+        }
+    }
+    public int i = 0;
+    @Override
+    public CustomAdapter.CustomViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        //create a new view
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View v = inflater.inflate(R.layout.row_layout_constrained, parent, false);
+        //set the view's size, margins, padding, and layout parameters
+        CustomViewHolder vh = new CustomViewHolder(v);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(CustomViewHolder holder, final int position) {
+        //get element from dataset at this posiiton
+        //replace the contents of the view with that element
+        final  String name = values.get(position);
+        holder.txtPersonName.setText(name);
+        //here you could have a setOnClickLister that calls another method if you want
+        if(position%8==0) {
+            holder.txtPersonName.setText("Bill Gates");
+            holder.txtNumPeople.setText("57");
+            holder.picOfP.setImageResource(R.mipmap.bill_gates);
+            holder.txtetDescription.setText("dinner # " + name +" with Elon to talk about telsa");
+            holder.vrtLine.setBackgroundColor(Color.CYAN);
+            holder.txtDate.setText(R.string.friday_resource);
+            holder.txtTime.setText(R.string.time_resource);
+            holder.itemView.setElevation(20);
+            holder.hrLine.setBackgroundColor(Color.WHITE);
+
+        } else if(position%7==0) {
+            holder.txtPersonName.setText("Code Group");
+            holder.txtNumPeople.setText("100+");
+            holder.picOfP.setImageResource(R.mipmap.code_group);
+            holder.txtetDescription.setText("group discuption # " + name +" with Elon");
+            holder.vrtLine.setBackgroundColor(Color.YELLOW);
+            holder.txtDate.setText(R.string.friday_resource);
+            holder.txtTime.setText(R.string.time_resource);
+
+        } else if(position%6==0) {
+            holder.txtPersonName.setText("Luke Jones");
+            holder.txtNumPeople.setText("1");
+            holder.picOfP.setImageResource(R.mipmap.luke_face);
+            holder.txtetDescription.setText("walk the # " + name +" with bla bla bla ");
+            holder.vrtLine.setBackgroundColor(Color.RED);
+            holder.txtDate.setText(R.string.friday_resource);
+            holder.txtTime.setText(R.string.time_resource);
+
+        } else if(position%5==0) {
+            holder.txtPersonName.setText("Dad");
+            holder.txtNumPeople.setText("3");
+            holder.picOfP.setImageResource(R.mipmap.dad_face);
+            holder.txtetDescription.setText("Talking # " + name +" with dad over the new car aka lambo");
+            holder.vrtLine.setBackgroundColor(Color.DKGRAY);
+            holder.txtDate.setText(R.string.friday_resource);
+            holder.txtTime.setText(R.string.time_resource);
+
+        } else if(position%4==0) {
+            holder.txtPersonName.setText("Computer Group");
+            holder.txtNumPeople.setText("5");
+            holder.picOfP.setImageResource(R.mipmap.computer_group);
+            holder.txtetDescription.setText("Meeting # " + name +" with Elon");
+            holder.vrtLine.setBackgroundColor(Color.RED);
+            holder.txtDate.setText(R.string.friday_resource);
+            holder.txtTime.setText(R.string.time_resource);
+
+        } else if(position%3==0) {
+            holder.txtPersonName.setText("Elon Musk");
+            holder.txtNumPeople.setText("77");
+            holder.picOfP.setImageResource(R.mipmap.elon_musk);
+            holder.txtetDescription.setText("going to mars # " + name +" with Elon");
+            holder.vrtLine.setBackgroundColor(Color.GREEN);
+            holder.txtDate.setText(R.string.friday_resource);
+            holder.txtTime.setText(R.string.time_resource);
+
+        } else if(position%2==0) {
+            holder.txtPersonName.setText("Richard Branson");
+            holder.txtNumPeople.setText("11");
+            holder.picOfP.setImageResource(R.mipmap.richard_branson);
+            holder.txtetDescription.setText("fun stuff # " + name +" with Elon");
+            holder.vrtLine.setBackgroundColor(Color.MAGENTA);
+            holder.txtDate.setText(R.string.friday_resource);
+            holder.txtTime.setText(R.string.time_resource);
+
+        } else {
+            holder.txtPersonName.setText("Ladd Jones");
+            holder.txtNumPeople.setText("17");
+            holder.picOfP.setImageResource(R.mipmap.ladd_face);
+            holder.txtetDescription.setText("here we go # " + name +" with Elon");
+            holder.vrtLine.setBackgroundColor(Color.RED);
+            holder.txtDate.setText(R.string.friday_resource);
+            holder.txtTime.setText(R.string.time_resource);
+
+        }
+        holder.txtetDescription.setElevation(10);
+        holder.hrLine.setBackgroundColor(Color.WHITE);
+        holder.vrtLine.setBackgroundColor(Color.WHITE);
+        holder.vrtLine.setBackgroundColor(Color.LTGRAY);
+        holder.txtNumPeople.setTextColor(Color.WHITE);
+    }
+
+    @Override
+    public int getItemCount() {
+        return values.size();
+    }
+}
---
+++ GitSocialCalendar/app/src/main/java/laddjones/gitsocialcalendar/MainActivity.java
@@ -0,0 +1,48 @@
+package laddjones.gitsocialcalendar;
+
+/**
+ * Created by laddjones on 7/12/17.
+ */
+import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+    private RecyclerView myRecyclerView;
+    private RecyclerView.Adapter myAdapter;
+    private RecyclerView.LayoutManager myLayoutManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState); // here super is being used to called the parent method
+        setContentView(R.layout.activity_main);
+
+        //create toolbar
+        toolbar = (Toolbar) findViewById(R.id.tool_bar);
+        //setSupportActionBar(toolbar);
+
+        //set up the RecyclerView, first tell it where to find the xml to set it up
+        myRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+        //now set up the layout manager as a LinearLayoutManager
+        //'this' is referring to our current object that we want to set the layout mananger up for
+        myLayoutManager = new LinearLayoutManager(this);
+        //now set the RecyclerView's layout
+        myRecyclerView.setLayoutManager(myLayoutManager);
+        //now create the data that will be going in
+        List<String> input = new ArrayList<>();
+        for(int i = 0; i < 20; i++) {
+            input.add("Ladd Jones" + i);
+        }
+        //now set define an adapter and pass in the input
+        myAdapter = new CustomAdapter(input);
+        //now set the RecyclerView's adapter
+        myRecyclerView.setAdapter(myAdapter);
+    }
+}
